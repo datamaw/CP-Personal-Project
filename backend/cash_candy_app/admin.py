0484a05a752df7488cc3fcf676c82e058f6cb03a
datamaw <mawalton@gmail.com>
2021-12-18 18:46:24 -0500

    folder changes

--- backend/cash_candy_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from cash_candy_app.models import Child, WishList, Item, AllowanceList, AllowanceDetail 
-
-# Register your models here.
-allModels = [Child, WishList, Item, AllowanceList, AllowanceDetail]
-admin.site.register(allModels)
--- backend/cash_candy_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CashCandyAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cash_candy_app'
